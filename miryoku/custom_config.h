4b391e4fe394b4a6b6a0d939541dfdcda87dae37
Cory Ebner <coryebner@gmail.com>
2023-08-25 22:35:51 -0600

    Swap middle and outer thumb on corne.

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -56,7 +56,7 @@ MIRYOKU_X(GAME_MOUSE, "Game Mouse")
 XXX  K00  K01  K02  K03  K04       K05  K06  K07  K08  K09  &to U_BASE \
 XXX  K10  K11  K12  K13  K14       K15  K16  K17  K18  K19  &to U_GAME \
 XXX  K20  K21  K22  K23  K24       K25  K26  K27  K28  K29  XXX \
-               K32  K33  K34       K35  K36  K37
+               K32  K34  K33       K36  K35  K37
 
 #define MIRYOKU_LAYOUTMAPPING_TBKMINI( \
      K00, K01, K02, K03, K04,      K05, K06, K07, K08, K09, \
