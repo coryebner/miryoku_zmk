a32b5e464bc33c87b57d2e0249b15ca518df38a6
Cory Ebner <coryebner@gmail.com>
2023-10-22 14:24:08 -0600

    Reverted corne thumb cluster order to miryoku default

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -56,7 +56,7 @@ MIRYOKU_X(GAME_MOUSE, "Game Mouse")
 XXX  K00  K01  K02  K03  K04       K05  K06  K07  K08  K09  &to U_BASE \
 XXX  K10  K11  K12  K13  K14       K15  K16  K17  K18  K19  &to U_GAME \
 XXX  K20  K21  K22  K23  K24       K25  K26  K27  K28  K29  XXX \
-               K32  K34  K33       K36  K35  K37
+               K32  K33  K34       K35  K36  K37
 
 #define MIRYOKU_LAYOUTMAPPING_TBKMINI( \
      K00, K01, K02, K03, K04,      K05, K06, K07, K08, K09, \
@@ -92,7 +92,7 @@ XXX  K20  K21  K22  K23  K24       K25  K26  K27  K28  K29  XXX \
      &kp N1  K00  K01  K02  K03  K04       K05  K06  K07  K08  K09  &trans \
      &kp G   K10  K11  K12  K13  K14       K15  K16  K17  K18  K19  &trans \
      &kp V   K20  K21  K22  K23  K24       K25  K26  K27  K28  K29  XXX \
-                       K32  K34  K33       K36  K35  K37
+                       K32  K33  K34       K35  K36  K37
 
      #define MIRYOKU_LAYERMAPPING_GAME_MEDIA( \
           K00, K01, K02, K03, K04,      K05, K06, K07, K08, K09, \
@@ -103,7 +103,7 @@ XXX  K20  K21  K22  K23  K24       K25  K26  K27  K28  K29  XXX \
      &kp ESC   K00  K01  K02  K03  K04       K05  K06  K07  K08  K09  &trans \
      &kp GRAVE K10  K11  K12  K13  K14       K15  K16  K17  K18  K19  &trans \
      &kp LALT  K20  K21  K22  K23  K24       K25  K26  K27  K28  K29  XXX \
-                         K32  K34  K33       K36  K35  K37
+                         K32  K33  K34       K35  K36  K37
 
      #define MIRYOKU_LAYERMAPPING_GAME_MOUSE( \
           K00, K01, K02, K03, K04,      K05, K06, K07, K08, K09, \
@@ -114,7 +114,7 @@ XXX  K20  K21  K22  K23  K24       K25  K26  K27  K28  K29  XXX \
      &kp F1   K00  K01  K02  K03  K04       K05  K06  K07  K08  K09  &trans \
      &kp F13  K10  K11  K12  K13  K14       K15  K16  K17  K18  K19  &trans \
      &kp F14  K20  K21  K22  K23  K24       K25  K26  K27  K28  K29  XXX \
-                        K32  K34  K33       K36  K35  K37
+                        K32  K33  K34       K35  K36  K37
 #endif
 
 #if defined MIRYOKU_KEYBOARD_TBKMINI_ADAPTERV2
