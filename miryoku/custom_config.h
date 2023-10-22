ac2f346aae17d10e228561548e2fff9fe01f5420
Cory Ebner <coryebner@gmail.com>
2023-10-22 14:51:07 -0600

    Updated game keybinds

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -1,19 +1,19 @@
 #define MIRYOKU_LAYER_GAME \
-&kp N2,        &kp N3,            &kp N4,                &kp N5,            &kp R,                   &kp Y,             &kp U,             &kp I,             &kp O,             &kp P,     \
-&kp LSHFT,     &kp A,             &kp W,                 &kp D,             &kp F,                   &kp H,             &kp J,             &kp K,             &kp L,             &kp SQT,   \
-&kp LCTRL,     &kp Q,             &kp S,                 &kp E,             &kp B,                   &kp N,             &kp M,             &kp COMMA,         &kp DOT,           &kp SLASH, \
-U_NP,          U_NP,              U_LT(U_GAME_MEDIA, G), &kp SPACE,         U_LT(U_GAME_MOUSE, V),   &kp ENTER,         &kp BSPC,          &kp DEL,           U_NP,              U_NP
+&kp N2,        &kp N3,            &kp N4,                  &kp N5,          &kp R,                   &kp Y,             &kp U,             &kp I,             &kp O,             &kp P,     \
+&kp LSHFT,     &kp A,             &kp W,                   &kp D,           &kp F,                   &kp H,             &kp J,             &kp K,             &kp L,             &kp SQT,   \
+&kp LCTRL,     &kp Q,             &kp S,                   &kp E,           &kp G,                   &kp N,             &kp M,             &kp COMMA,         &kp DOT,           &kp SLASH, \
+U_NP,          U_NP,              U_LT(U_GAME_MEDIA, Z),   &kp SPACE,       U_LT(U_GAME_MOUSE, X),   &kp ENTER,         &kp BSPC,          &kp DEL,           U_NP,              U_NP
 
 #define MIRYOKU_LAYER_GAME_MEDIA \
 &kp F2,        &kp F3,            &kp F4,                &kp F5,            &kp F6,               U_NA,              U_NA,             U_NA,             U_NA,             U_NA, \
 &kp LSHFT,     &kp A,             &kp W,                 &kp D,             &kp I,                U_NA,              U_NA,             U_NA,             U_NA,             U_NA, \
-&kp LCTRL,     &kp C,             &kp S,                 &kp M,             &kp TAB,              U_NA,              U_NA,             U_NA,             U_NA,             U_NA, \
+&kp LCTRL,     &kp C,             &kp S,                 &kp V,             &kp B,              U_NA,              U_NA,             U_NA,             U_NA,             U_NA, \
 U_NP,          U_NP,              U_NA,                  U_NA,              U_NA,                 U_NA,              U_NA,             U_NA,             U_NA,             U_NA   
 
 #define MIRYOKU_LAYER_GAME_MOUSE \
-&kp N6,        &kp N7,            &kp N8,                &kp KP_NUMLOCK,    &kp T,                U_NA,              U_NA,             U_NA,             U_NA,             U_NA, \
+&kp N6,        &kp N7,            &kp N8,                &kp N9,            &kp T,                U_NA,              U_NA,             U_NA,             U_NA,             U_NA, \
 &kp LSHFT,     &kp A,             &kp W,                 &kp D,             &kp H,                U_NA,              U_NA,             U_NA,             U_NA,             U_NA, \
-&kp LCTRL,     &kp Z,             &kp S,                 &kp X,             &kp N,                U_NA,              U_NA,             U_NA,             U_NA,             U_NA, \
+&kp LCTRL,     &kp N,             &kp S,                 &kp M,             &kp COMMA,                U_NA,              U_NA,             U_NA,             U_NA,             U_NA, \
 U_NP,          U_NP,              U_NA,                  U_NA,              U_NA,                 U_NA,              U_NA,             U_NA,             U_NA,             U_NA
 
 #define MIRYOKU_LAYER_LIST \
@@ -89,10 +89,10 @@ XXX  K20  K21  K22  K23  K24       K25  K26  K27  K28  K29  XXX \
           K20, K21, K22, K23, K24,      K25, K26, K27, K28, K29, \
           N30, N31, K32, K33, K34,      K35, K36, K37, N38, N39 \
      ) \
-     &kp N1  K00  K01  K02  K03  K04       K05  K06  K07  K08  K09  &trans \
-     &kp G   K10  K11  K12  K13  K14       K15  K16  K17  K18  K19  &trans \
-     &kp V   K20  K21  K22  K23  K24       K25  K26  K27  K28  K29  XXX \
-                       K32  K33  K34       K35  K36  K37
+     &kp N1   K00  K01  K02  K03  K04       K05  K06  K07  K08  K09  &trans \
+     &kp TAB  K10  K11  K12  K13  K14       K15  K16  K17  K18  K19  &trans \
+     &kp LALT K20  K21  K22  K23  K24       K25  K26  K27  K28  K29  XXX \
+                        K32  K33  K34       K35  K36  K37
 
      #define MIRYOKU_LAYERMAPPING_GAME_MEDIA( \
           K00, K01, K02, K03, K04,      K05, K06, K07, K08, K09, \
@@ -102,7 +102,7 @@ XXX  K20  K21  K22  K23  K24       K25  K26  K27  K28  K29  XXX \
      ) \
      &kp ESC   K00  K01  K02  K03  K04       K05  K06  K07  K08  K09  &trans \
      &kp GRAVE K10  K11  K12  K13  K14       K15  K16  K17  K18  K19  &trans \
-     &kp LALT  K20  K21  K22  K23  K24       K25  K26  K27  K28  K29  XXX \
+     &kp Z     K20  K21  K22  K23  K24       K25  K26  K27  K28  K29  XXX \
                          K32  K33  K34       K35  K36  K37
 
      #define MIRYOKU_LAYERMAPPING_GAME_MOUSE( \
@@ -111,10 +111,10 @@ XXX  K20  K21  K22  K23  K24       K25  K26  K27  K28  K29  XXX \
           K20, K21, K22, K23, K24,      K25, K26, K27, K28, K29, \
           N30, N31, K32, K33, K34,      K35, K36, K37, N38, N39 \
      ) \
-     &kp F1   K00  K01  K02  K03  K04       K05  K06  K07  K08  K09  &trans \
-     &kp F13  K10  K11  K12  K13  K14       K15  K16  K17  K18  K19  &trans \
-     &kp F14  K20  K21  K22  K23  K24       K25  K26  K27  K28  K29  XXX \
-                        K32  K33  K34       K35  K36  K37
+     &kp F1          K00  K01  K02  K03  K04       K05  K06  K07  K08  K09  &trans \
+     &kp KP_NUMLOCK  K10  K11  K12  K13  K14       K15  K16  K17  K18  K19  &trans \
+     &kp X           K20  K21  K22  K23  K24       K25  K26  K27  K28  K29  XXX \
+                               K32  K33  K34       K35  K36  K37
 #endif
 
 #if defined MIRYOKU_KEYBOARD_TBKMINI_ADAPTERV2
